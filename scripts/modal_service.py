818078f9540763a81704ac50fc4717c9ba3114cf
Dmitry B <dmitry.bezyazychnyy@careem.com>
2026-04-13 21:43:10 +0500

    up modal params

--- scripts/modal_service.py
+++ scripts/modal_service.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 import modal
 
+CONFIG_FILE = "configs/hf-example.yaml"
+SECOND = 1
+MINUTE = 60 * SECOND
 
 image = (
     modal.Image.debian_slim()
@@ -9,10 +12,8 @@ image = (
     .uv_pip_install(
         "jax-server[jax] @ git+https://github.com/dmitryBe/jax-server.git@main"
     )
-    .add_local_file(
-        "configs/hf-example.yaml", "/root/configs/hf-example.yaml", copy=True
-    )
-    .env({"JAX_SERVER_CONFIG": "/root/configs/hf-example.yaml"})
+    .add_local_file(CONFIG_FILE, f"/root/{CONFIG_FILE}", copy=True)
+    .env({"JAX_SERVER_CONFIG": f"/root/{CONFIG_FILE}"})
 )
 
 app = modal.App("jax-server-example")
@@ -21,6 +22,10 @@ app = modal.App("jax-server-example")
 @app.function(
     image=image,
     secrets=[modal.Secret.from_name("huggingface-token", required_keys=["HF_TOKEN"])],
+    enable_memory_snapshot=True,
+    max_containers=3,
+    scaledown_window=1 * MINUTE,
+    timeout=100 * SECOND,
 )
 @modal.asgi_app(label="jax-server-example")
 def fastapi_app():
