d40e58cef1306eba87c5082235b34e7ff151c354
Akhtar Nadeem <muhammad.nadeem@careem.com>
2026-06-06 01:39:02 +0500

    add logging

--- src/jax_server/deploy/k8s_main.py
+++ src/jax_server/deploy/k8s_main.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
+import logging
 import os
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(name)s %(levelname)s %(message)s",
+)
 
 from jax_server.server.app import create_app
 
